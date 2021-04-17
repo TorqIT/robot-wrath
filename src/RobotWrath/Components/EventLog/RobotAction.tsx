566f77705634f12c981ef0c5a4ac77d1470fe065
Nick <nmacdonald@compassaq.com>
2021-04-17 11:04:35 -0300

    -Added icons in place of the event log text

--- src/RobotWrath/Components/EventLog/RobotAction.tsx
+++ src/RobotWrath/Components/EventLog/RobotAction.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Robot, RobotCombatant, TurnEvent } from "../../interfaces";
 import { RobotIcon } from "../RobotIcon";
 import styles from "./eventLog.module.css";
+import attackIcon from "./TurnIcons/attack.png";
+import blockIcon from "./TurnIcons/block.png";
+import badBlockIcon from "./TurnIcons/badBlock.png";
 
 export interface RobotActionProps {
   attackers: RobotCombatant[];
@@ -22,11 +25,15 @@ const RobotAction: React.FC<RobotActionProps> = ({
         ))}
       </div>
       <div style={{ flex: 1, textAlign: "center" }}>
-        {isBlocking
-          ? attackers.length == 0
-            ? "OOPS"
-            : "BLOCKED BY"
-          : "ATTACKED"}
+        <img
+          src={
+            isBlocking
+              ? attackers.length == 0
+                ? badBlockIcon
+                : blockIcon
+              : attackIcon
+          }
+        />
       </div>
       <div>
         <RobotIcon robot={target} small />
